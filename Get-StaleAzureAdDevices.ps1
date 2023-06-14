967c3236f114979ee6588826e01242b318c4f94b
bentman <26147320+bentman@users.noreply.github.com>
2023-06-14 14:34:38 -0500

    Add files via upload

---
+++ Get-StaleAzureAdDevices.ps1
@@ -0,0 +1,99 @@
+<#
+.SYNOPSIS
+This script identifies stale devices in Azure AD.
+
+.DESCRIPTION
+The script authenticates with an Azure AD App Registration and uses the Microsoft Graph API to retrieve all devices in Azure AD. 
+It then filters these devices based on the 'approximateLastLogonTimestamp' property to identify devices that have not been used for a specified number of days.
+
+.PARAMETER clientId
+The client ID of the Azure AD App Registration.
+.PARAMETER tenantId
+The tenant ID of the Azure AD tenant.
+.PARAMETER clientSecret
+The client secret of the Azure AD App Registration.
+.PARAMETER staleDays
+The period of inactivity (in days) after which a device should be considered stale.
+
+.EXAMPLE
+.\Get-StaleAzureAdDevices.ps1 -clientId 'your-client-id' -tenantId 'your-tenant-id' -clientSecret 'your-client-secret' -staleDays 30
+
+.NOTES
+The script uses the MSAL.PS PowerShell module to authenticate with Azure AD. 
+Make sure this module is installed before running the script.
+
+.SETUP
+To create a Runbook in Azure Automation with this script, follow these steps:
+
+1. Open the Azure portal and navigate to your Automation Account.
+2. Under "Process Automation", select "Runbooks".
+3. Click on "+ Create a runbook".
+4. Enter a name for the Runbook (e.g., "Get-StaleAzureAdDevices"), select "PowerShell" as the Runbook type, and click on "Create".
+5. In the editor that opens, paste the PowerShell script.
+6. Click on "Save".
+7. To test the Runbook, click on "Start" and enter the required parameters in the pane that appears on the right. 
+    These parameters are the client ID, tenant ID, client secret, and stale days.
+8. Click on "OK" to start the Runbook. The output will appear in the pane at the bottom of the screen.
+9. After testing, remember to publish the Runbook by clicking on "Publish".
+
+The Runbook can now be scheduled to run at specific times or triggered by specific events. 
+For logging and audit trails, you can use the "Jobs" under "Process Automation" in your Automation Account. 
+This will show you a history of all the Runbook jobs, including their status, start time, end time, and any errors or output.
+Azure Automation Documentation
+https://docs.microsoft.com/en-us/azure/automation/
+
+Azure AD App Registration: Create a client secret
+https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal#create-a-new-application-secret
+#>
+
+param (
+    # The client ID of your Azure AD App Registration
+    [Parameter(Mandatory=$true)][string] $clientId,
+    # The tenant ID of your Azure AD tenant
+    [Parameter(Mandatory=$true)][string] $tenantId,
+    # The client secret of your Azure AD App Registration
+    [Parameter(Mandatory=$true)][string] $clientSecret,
+    # The period of inactivity (in days) after which a device should be considered stale
+    [Parameter(Mandatory=$true)][int] $staleDays
+)
+# Import the MSAL.PS module
+try {
+    Install-Module -Name MSAL.PS -Force
+} catch {
+    Write-Error "Failed to import MSAL.PS module. Error: $($_.Exception.Message)"
+    return
+}
+# Authenticate with your App Registration and get an access token
+try {
+    $token = Get-MsalToken `
+        -ClientId $clientId `
+        -TenantId $tenantId `
+        -ClientSecret $clientSecret
+} catch {
+    Write-Error "Failed to get access token. Error: $($_.Exception.Message)"
+    return
+}
+# Calculate the date before which a device should be considered stale
+$stalePeriod = (Get-Date).AddDays(-$staleDays)
+# Define the URI for the Microsoft Graph API endpoint to get all devices
+$uri = "https://graph.microsoft.com/v1.0/devices"
+# Define the headers for the API request
+$headers = @{
+    "Authorization" = "Bearer $($token.AccessToken)"
+}
+# Send a GET request to the API and get the response
+try {
+    $response = Invoke-RestMethod `
+        -Uri $uri `
+        -Headers $headers
+} catch {
+    Write-Error "Failed to get devices. Error: $($_.Exception.Message)"
+    return
+}
+# Extract the devices from the response
+$devices = $response.value
+# Filter the devices based on the 'approximateLastLogonTimestamp' property
+$staleDevices = $devices | 
+    Where-Object { $_.approximateLastLogonTimestamp -le $stalePeriod }
+# Output the stale devices
+$staleDevices
---
+++ Manage-StaleAzureAdDevices.ps1
@@ -0,0 +1,60 @@
+<#
+.SYNOPSIS
+This script starts the Get-StaleAzureAdDevices and Remove-StaleAzureAdDevices runbooks, effectively managing the detection and removal of stale Azure AD devices.
+
+.DESCRIPTION
+The Manage-StaleAzureAdDevices script is designed to manage stale Azure AD devices. 
+It starts the Get-StaleAzureAdDevices and Remove-StaleAzureAdDevices runbooks, detects stale devices, and removes them.
+
+.PARAMETER clientId
+The client ID of your Azure AD App Registration.
+.PARAMETER tenantId
+The tenant ID of your Azure AD tenant.
+.PARAMETER clientSecret
+The client secret of your Azure AD App Registration.
+.PARAMETER staleDays
+The period of inactivity (in days) after which a device should be considered stale.
+
+.EXAMPLE
+.\Manage-StaleAzureAdDevices.ps1 -clientId '<client_id>' -tenantId '<tenant_id>' -clientSecret '<client_secret>' -staleDays 90
+
+.NOTES
+This script should be executed in an environment where the Azure PowerShell module is installed and you are authenticated with an account that has the necessary permissions.
+
+.LINK
+Azure Automation Documentation
+https://docs.microsoft.com/en-us/azure/automation/
+
+Azure AD App Registration: Create a client secret
+https://docs.microsoft.com/en-us/azure/active-directory/develop/howto-create-service-principal-portal#create-a-new-application-secret
+
+Start-AzAutomationRunbook Documentation
+https://docs.microsoft.com/en-us/powershell/module/az.automation/start-azautomationrunbook
+#>
+
+param (
+    # The client ID of your Azure AD App Registration
+    [Parameter(Mandatory=$true)][string] $clientId,
+    # The tenant ID of your Azure AD tenant
+    [Parameter(Mandatory=$true)][string] $tenantId,
+    # The client secret of your Azure AD App Registration
+    [Parameter(Mandatory=$true)][string] $clientSecret,
+    # The period of inactivity (in days) after which a device should be considered stale
+    [Parameter(Mandatory=$true)][int] $staleDays
+)
+# Start the Get-StaleAzureAdDevices runbook and get its output
+$getJob = (
+    Start-AzAutomationRunbook `
+        -AutomationAccountName 'YourAutomationAccountName' `
+        -Name 'Get-StaleAzureAdDevices' `
+        -Parameters @{ 
+            'clientId' = $clientId; 'tenantId' = $tenantId; 'clientSecret' = $clientSecret; 'staleDays' = $staleDays 
+        } -MaxWaitSeconds 3600 -Wait)
+$staleDevices = $getJob.Output
+# Start the Remove-StaleAzureAdDevices runbook with the output of the Get-StaleAzureAdDevices runbook
+Start-AzAutomationRunbook `
+    -AutomationAccountName 'YourAutomationAccountName' `
+    -Name 'Remove-StaleAzureAdDevices' `
+    -Parameters @{ 
+        'clientId' = $clientId; 'tenantId' = $tenantId; 'clientSecret' = $clientSecret; 'staleDevices' = $staleDevices 
+    } -MaxWaitSeconds 3600 -Wait
